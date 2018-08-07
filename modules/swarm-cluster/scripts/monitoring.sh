1b5a998cadf81120796cd7b31bd7ac8b983cd6b2
Juncheol Cho <zironycho@gmail.com>
2018-08-07 12:35:54 +0900

    update portainer for swarm agents

--- modules/swarm-cluster/scripts/monitoring.sh
+++ modules/swarm-cluster/scripts/monitoring.sh
@@ -22,14 +22,10 @@ docker service create -d --name viz \
   --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   dockersamples/visualizer
 
-docker service create -d --name portainer \
-  --constraint 'node.role == manager' \
-  --label "traefik.backend=portainer" \
-  --label "traefik.port=9000" \
-  --label "traefik.frontend.rule=Host:${HOST};PathPrefixStrip:/portainer" \
-  --network proxy \
-  --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
-  portainer/portainer
+curl -L https://gist.github.com/zironycho/0e53a5e4e2df1e29aed845dba94be64c/raw/ \
+  -o portainer-agent-stack-traefik.yml
+
+docker stack deploy -c portainer-agent-stack-traefik.yml portainer
 
 git clone https://github.com/zironycho/swarmprom.git
 cd swarmprom
